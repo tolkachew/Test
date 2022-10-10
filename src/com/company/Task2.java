ae866cdebf5c4dbf737ce9697351d4e6630d035a
гк <tolkachew.wlad@yandex.ru>
2022-10-10 19:20:10 +0300

    change smth

--- src/com/company/Task2.java
+++ src/com/company/Task2.java
@@ -6,18 +6,17 @@ import java.util.Scanner;
 public class Task2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int a = 0, b = 0;
+        int count = 0,a = 0, b = 0;
         for (int i = 1; i <= 10; i++) {
             if (i % 2 == 0 && i % 3 == 0){
-                a++;
-                b++;
+                count++;
             }else if (i%2 ==0){
-                a++;
+                count++;
             }else if (i%3 ==0){
-                b++;
+                count++;
             }
         }
-        System.out.printf("The number of numbers that are divisible by 2 : %d, by 3 : %d",a,b);
+        System.out.printf("The number of numbers that are divisible by 2 and 3 : %d",count);
     }
 }
 
